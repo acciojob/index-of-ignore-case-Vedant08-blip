e2d55d7ebfa95799f749a622096c0c8825720f05
Vedant08-blip <vedanttrivedi87@gmail.com>
2025-06-21 12:55:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+  if (!s2) return 0; // Empty substring should return index 0
+  return s1.toLowerCase().indexOf(s2.toLowerCase());
 }
 
 // Please do not change the code below
